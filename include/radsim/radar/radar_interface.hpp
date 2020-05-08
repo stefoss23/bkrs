8c95687ae16b944c17daf051ba4b2ee61c7f351a
Steinar Foss <stefoss23@gmail.com>
2020-05-08 23:57:24 +0200

    added radar_interface.

---
+++ include/radsim/radar/radar_interface.hpp
@@ -0,0 +1,82 @@
+/*
+The RadarCom object is the interface between the radar and the processing during real time data processing. 
+
+The RadarCom object own the radar object. 
+
+It is initialized by:
+
+RadarComType com(RadarConfig, target_collection):
+
+Simulation is started bv:
+com.start() in which data will regularly be inserted into a queue and are readily available. 
+
+com.stop() stops the simulation. 
+*/
+
+#ifndef RADAR_INTERFACE_HPP
+#define RADAR_INTERFACE_HPP
+
+#include <vector>
+#include <thread>
+#include <atomic>
+
+#include <radsim/radar/target.hpp>
+#include <radsim/radar/radar_config.hpp>
+#include <radsim/radar/radar_data_queue.hpp>
+#include <radsim/radar/radar.hpp>
+
+namespace radsim {
+
+class RadarInterface {
+
+  Radar radar;
+  TargetCollection target_collection;
+
+  std::thread * sim_thread;
+  RadarDataQueue queue;
+
+  std::atomic<double> sim_time; //s, the "Clock" of the simulator
+  std::atomic<bool> allow_send_data; //allows for sending data from sim to process    
+  std::atomic<bool> on;            //on tells the sim_runner to continue running
+
+  bool has_run;       //simulator has run once, and cannot restart
+  bool statistics;   //If true, at end of a sim_run, Percentage work time is printed, 
+  double time_step;    //s, time_step in simulation before updating sim_time;
+  size_t queue_size; //number of elements in queue is at least this number
+
+  //radar parameters
+  double min_range; //m
+  double range_bin; //m
+
+  public:
+    RadarInterface(const RadarConfig& config, TargetCollection target_collection_, double dt = 0.15);
+    //dt: s, timestep in the simulation, before updating sim_time. 
+
+    RadarInterface(const RadarInterface& other) = delete;
+    RadarInterface& operator=(const RadarInterface& other) = delete;
+
+    ~RadarInterface();
+
+    void setStatistics(bool set);
+    //set: if true, A printout of how much simulator thread worked, is printed. Default set = false
+
+    void setAddNoise(bool set);
+    //set: if yes: raadar receiver noise is added to simulation, default is true
+
+    void start(bool signal_override = false, double signal_strength = 0);
+    //signal_override: if yes, then received signal is signal_strength.
+    //signal_strength = 0
+
+    void stop();
+    
+    double getSimTime() const; //s
+
+    bool dataReady();
+    PulseData getData();
+    double getRange(int bin_index) const; //m
+
+};
+
+}
+
+#endif
---
+++ radar/src/radar_interface.cpp
@@ -0,0 +1,185 @@
+
+#include <radsim/utils/timer.hpp>
+
+#include <radsim/radar/target.hpp>
+#include <radsim/radar/radar.hpp>
+#include <radsim/radar/radar_config.hpp>
+#include <radsim/radar/radar_interface.hpp>
+
+using namespace std;
+using namespace radsim;
+
+namespace {
+
+  void simulationRunner(Radar& radar,
+                        RadarDataQueue& queue,
+                        const TargetCollection& targets,
+                        double time_step, 
+                        atomic<double>& sim_time_atomic, 
+                        atomic<bool>& on, 
+                        atomic<bool>& allow_send_data, 
+                        bool signal_override, 
+                        double signal_strength,
+                        bool statistics) {
+
+    radar.reset(0);  //sim_time reset to zero
+    double sim_check = time_step; //s, 
+    double start_time = radar.getCurrentTime();
+    sim_time_atomic.store( start_time ); //s
+
+    //If the queue does not contain some elements, it may falter during oepration
+    queue.push_initial( radar.generatePulseData(targets, signal_override, signal_strength) );
+
+    allow_send_data.store(true);  
+
+    double work_time = 0; //s, The time the thread has worked without busy-wait
+    Timer timer;
+    double current_time = 0;
+
+    while(on.load()) {
+
+      double period_start = timer.elapsed(); //s
+
+      do {
+        queue.push( radar.generatePulseData(targets, signal_override, signal_strength) );
+      } while (radar.getCurrentTime() < sim_check );
+
+      current_time = radar.getCurrentTime(); //s  
+      sim_time_atomic.store(current_time); //s  
+
+      work_time += (timer.elapsed() - period_start);
+
+      while (timer.elapsed() < radar.getCurrentTime()) {} //busy-wait
+
+      sim_check += time_step; //s
+    }
+    double end_time = timer.elapsed();
+
+    if (statistics) {
+      double frac_work = 100.0 * work_time / radar.getCurrentTime(); //unit, fraction of simulation time spent on actual calculations.
+      cout << "Simulation Work Fraction(%): " << frac_work << endl;
+    }
+   
+  }
+
+} //end empty namespace
+
+
+namespace radsim {
+
+
+RadarInterface::RadarInterface(const RadarConfig& config, TargetCollection target_collection_arg, double dt) :
+  radar( config ),
+  target_collection( move(target_collection_arg) ),
+  sim_thread(NULL),
+  allow_send_data(false),
+  on(false),
+  has_run(false),
+  statistics(false),
+  time_step(dt),
+  sim_time(0),
+  queue_size(0)
+{
+  min_range = radar.getMinimumRange(); //m
+  range_bin = radar.getRangeBin(); //m
+}
+
+RadarInterface::~RadarInterface() {
+  stop();
+}
+
+
+void RadarInterface::setStatistics(bool set) {
+  if (sim_thread)
+    throw logic_error(__PRETTY_FUNCTION__ + string(": cannot set Statistics when simulation thread is running."));
+
+  statistics = set;
+}
+
+
+void RadarInterface::setAddNoise(bool set) 
+{
+  if (sim_thread)
+    throw logic_error(__PRETTY_FUNCTION__ + string(": cannot set radar parameters when simulation thread is running."));
+
+  radar.setToAddNoise(set);
+}
+
+
+void RadarInterface::start(bool signal_override, double signal_strength) {
+
+  if (sim_thread)
+    throw logic_error(__PRETTY_FUNCTION__ + string(": cannot restart simulator without stopping first."));
+
+  if (has_run)
+    throw logic_error(__PRETTY_FUNCTION__ + string(": Interface obj can run simulations only once."));
+
+  has_run = true;
+  on.store(true);
+
+  sim_thread = new thread(simulationRunner, 
+                          ref(radar), ref(queue), 
+                          ref(target_collection), 
+                          time_step, 
+                          ref(sim_time), 
+                          ref(on), 
+                          ref(allow_send_data), 
+                          signal_override, 
+                          signal_strength, 
+                          statistics);
+
+}
+
+
+//s
+double RadarInterface::getSimTime() const {
+  if (allow_send_data.load()) {
+    double t = sim_time.load();
+    return t;
+  }
+  else
+    return 0.0;
+}
+
+
+bool RadarInterface::dataReady() {
+
+  if (queue_size > 1) 
+    return true;
+
+  if (allow_send_data.load())
+    queue_size = queue.size();
+
+  return (queue_size > 1);
+}
+
+PulseData RadarInterface::getData() {
+
+  if (queue_size > 1) {
+    queue_size--;
+    return queue.pop();
+  }
+  else
+    throw logic_error(__PRETTY_FUNCTION__ + string(": no data in queue. Check dataReady() first."));
+}
+
+void RadarInterface::stop() {
+
+  allow_send_data.store(false);  //this stops data exhange before sim is shut down
+  on.store(false);          //shutting down sim
+
+  //destroying thread
+  if (sim_thread) {
+    sim_thread->join();
+    delete sim_thread;
+    sim_thread = NULL;
+  }
+}
+
+//m
+double RadarInterface::getRange(int bin_index) const {
+  return min_range + bin_index * range_bin; //m
+}
+
+} //end namespace bkradsim
+
