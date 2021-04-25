e630a0e05de9d09d214a659180613e47cd82ef54
Steinar Foss <stefoss23@gmail.com>
2021-04-25 19:41:16 +0200

    added assert.hpp: function for test assertions.
    test_assert: made compilable.
    
    assert.hpp: first prototype.
    
    test_assert_double -> testAssertDouble.
    
    assert.hpp: new prototype.
    
    ...
    
    ...
    
    assert.hpp: added assertIntEqual.
    
    assert.hpp: added 'writeError.
    
    assert.hpp: added assertStringEqual
    
    assert.hpp: removed 'using'.
    
    assert.hpp: created class 'Assert'.
    
    assert.hpp: moved 'writeError' into 'Assert'.
    
    Assert: 'writeError' -> 'handleEqualError.
    
    Assert: added ErrorType.
    
    ...
    
    ...
    
    ...
    
    added assert.cpp.
    
    test_assert: added test for assertComplexEqual.
    
    assert.hpp: added assertComplexEqual.

---
+++ tests/utils/test_assert.cpp
@@ -0,0 +1,45 @@
+
+#include <exception>
+#include <complex>
+
+#include <radsim/utils/assert.hpp>
+
+using namespace std;
+using namespace radsim;
+
+//These tests need to be checked manually also, since
+//all failed assertions causes immediate program abortion. 
+
+void test_assert_double() {
+  assertDoubleEqual(1.0, 1.0, 1e-4);
+}
+
+void test_assert_complex() {
+  assertComplexEqual(-2.0+(1.0i), -2.0+(1.0i), 1e-4);
+}
+
+void test_assert_int() {
+  assertIntEqual(2, 2);
+}
+
+void test_assert_string() {
+  assertStringEqual("a", "a");
+}
+
+void test_assert_true_false() {
+  assertTrue(true);
+  
+
+  assertFalse(false);
+}
+
+int main() {
+
+  test_assert_double();
+  test_assert_complex();
+  test_assert_int();
+  test_assert_string();
+  test_assert_true_false();
+
+  return 0;
+}
