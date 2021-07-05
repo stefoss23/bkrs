f17b00221d161ab3ee3a2ff355f431450a442686
Steinar Foss <stefoss23@gmail.com>
2021-07-05 16:13:01 +0200

    test_target.cpp: removed assert.h include.

--- tests/radar/test_target.cpp
+++ tests/radar/test_target.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <assert.h>
 
 #include <memory>
 
-#include <radsim/utils/utils.hpp>
+#include <radsim/utils/assert.hpp>
 
 #include <radsim/mathematics/math_vector.hpp>
 #include <radsim/mathematics/approx_function.hpp>
@@ -16,11 +15,11 @@ using namespace radsim;
 void test_get_pos() {
   math_vector pos = {20, 20, 10}; //m
   Target target(pos, 8.0);
-  assert( double_equal(math_vector_length(target.getPosition()), 30, 1e-4) );
-  assert( target.getRCS() == 8.0 );
+  assertDoubleEqual(math_vector_length(target.getPosition()), 30, 1e-4);
+  assertIntEqual( target.getRCS(), 8.0 );
 
   target.setPosition({3, 4, 0});
-  assert( double_equal(math_vector_length(target.getPosition()), 5, 1e-4 ) ); 
+  assertDoubleEqual( math_vector_length(target.getPosition()), 5, 1e-4 ); 
 }
 
 void test_path() {
@@ -31,9 +30,9 @@ void test_path() {
   Target target(move(path), 8.0);
 
   math_vector middle = {0.5, 1.0, 1.5};
-  assert( target.getPosition(0) == start );
+  assertTrue( target.getPosition(0) == start );
 
-  assert( target.getPosition(0.5) == middle );
+  assertTrue( target.getPosition(0.5) == middle );
 }
 
 void test_move() {
@@ -47,8 +46,8 @@ void test_move() {
   const void * ptr_array = target.getPath().getValueVector()[0].data();
 
   Target copy = move(target);
-  assert( ptr_entry == copy.getPath().getEntryVector().data() );
-  assert( ptr_array == copy.getPath().getValueVector()[0].data() );
+  assertTrue( ptr_entry == copy.getPath().getEntryVector().data() );
+  assertTrue( ptr_array == copy.getPath().getValueVector()[0].data() );
 
 }
 
