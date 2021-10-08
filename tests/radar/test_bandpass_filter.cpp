79952f3aad676c91b1821860f8b8e17851a450b1
Steinar Foss <stefoss23@gmail.com>
2021-10-08 07:44:37 +0200

    test_bandpass_filter uses assert.hpp.

--- tests/radar/test_bandpass_filter.cpp
+++ tests/radar/test_bandpass_filter.cpp
@@ -1,4 +1,4 @@
-#include <assert.h>
+#include <radsim/utils/assert.hpp>
 
 #include <radsim/radar/bandpass_filter.hpp>
 
@@ -8,21 +8,21 @@ using namespace radsim;
 
 void test_standard() {
   auto H = createBandpassFilter(BandpassFilter::Standard, 2.0);
-  assert( complex_equal(H.output( 0.00), 1.0, 1e-3) );
-  assert( complex_equal(H.output( 0.50), 1.0, 1e-3) );
-  assert( complex_equal(H.output( 1.00), 0.5, 1e-3) );
-  assert( abs(H.output( 1.50)) < 1e-3 );
-  assert( complex_equal(H.output(-0.50), 1.0, 1e-3) );
-  assert( complex_equal(H.output(-1.00), 0.5, 1e-4) );
-  assert( abs(H.output(-1.50)) < 1e-3 );
-  assert( complex_equal(H.output( 1.25), 0.14644, 1e-3) );
+  assertComplexEqual( H.output( 0.00), 1.0, 1e-3 );
+  assertComplexEqual( H.output( 0.50), 1.0, 1e-3 );
+  assertComplexEqual( H.output( 1.00), 0.5, 1e-3 );
+  assertTrue( abs(H.output( 1.50)) < 1e-3 );
+  assertComplexEqual( H.output(-0.50), 1.0, 1e-3 );
+  assertComplexEqual( H.output(-1.00), 0.5, 1e-4 );
+  assertTrue( abs(H.output(-1.50)) < 1e-3 );
+  assertComplexEqual( H.output( 1.25), 0.14644, 1e-3 );
 }
 
 void test_rectangular() {
   auto H = createBandpassFilter(BandpassFilter::Rectangular, 2.0);
-  assert( complex_equal(H.output( 0.00), 1.0, 1e-3) );
-  assert( complex_equal(H.output( 0.99), 1.0, 1e-3) );  
-  assert( abs(H.output( 1.01)) < 1e-3 );
+  assertComplexEqual( H.output( 0.00), 1.0, 1e-3 );
+  assertComplexEqual( H.output( 0.99), 1.0, 1e-3 );  
+  assertTrue( abs(H.output( 1.01)) < 1e-3 );
 
 }
 
