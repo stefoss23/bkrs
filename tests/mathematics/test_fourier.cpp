4c7e6c09de7ecfdb1d22d4d26daab218c4f20298
Steinar Foss <stefoss23@gmail.com>
2021-11-15 07:39:30 +0100

    test_fourier uses assert.hpp.

--- tests/mathematics/test_fourier.cpp
+++ tests/mathematics/test_fourier.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
-#include <assert.h>
 #include <complex.h>
+
+#include <radsim/utils/assert.hpp>
 
 #include <radsim/mathematics/constants.hpp>
 #include <radsim/mathematics/fourier.hpp>
@@ -40,8 +41,8 @@ void test_fourier_from_table() {
   
   //The actual Fourier transform
   ComplexApproxFunction F = fourierTransform(G, f_entry, 4e-4, -3.0, 3.0);
-  assert( complex_equal(F.output(0.0), sqrt(pi),                        2e-3));
-  assert( complex_equal(F.output(0.5), exp(-0.25 * pi * pi) * sqrt(pi), 2e-3));
+  assertComplexEqual( F.output(0.0), sqrt(pi), 2e-3 );
+  assertComplexEqual( F.output(0.5), exp(-0.25 * pi * pi) * sqrt(pi), 2e-3 );
 }
 
 //Transform of ConstFunction from 0.0 to 2.0 yields: -1 / (2 * PI * i * f) * exp(-4 * PI * i * f)
@@ -53,10 +54,10 @@ void test_fourier_from_table_const_function() {
   
   std::complex<double> result;
   result = fourierTransformSingle(G, 1.0, 1e-4, 0.0, 2.0);
-  assert( abs(result) < 1e-3 );
+  assertTrue( abs(result) < 1e-3 );
   result = fourierTransformSingle(G, 0.5, 1e-4, 1.0, 2.0);
   std::complex<double> cmp = (2.0i) / pi; 
-  assert( complex_equal(result, cmp, 1e-2) );
+  assertComplexEqual( result, cmp, 1e-2 );
 }
 
 
