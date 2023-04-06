3001031fcf667af7a8bc17b4f3b60c220a33f015
Steinar Foss <stefoss23@gmail.com>
2023-04-06 18:43:08 +0200

    Added class defintion for PulseDataReader.

---
+++ include/radsim/radar/pulse_data_reader.hpp
@@ -0,0 +1,11 @@
+#ifndef RADAR_PULSE_DATA_READER_HPP
+#define RADAR_PULSE_DATA_READER_HPP
+
+class PulseDataReader {
+  public:
+    PulseDataReader(const std::string filename);
+    PulseData Read();
+    Close();
+};
+
+#endif
